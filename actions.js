43cc729888cd4511989b0c3f8b8c7ddf2802d6ea
m ungs <ungson001@live.com>
2021-10-14 22:43:35 -0700

    assignment completed by michael

---
+++ actions.js
@@ -0,0 +1,25 @@
+// Challenge 1
+
+/* for(i = 1; i < 21; i++) {
+    if(i % 3 == 0) {
+        console.log(i)
+    } 
+} */
+
+// Challenge 2
+
+/* for(i = 100; i > 0; i--) {
+    if(i % 3 == 0) {
+        console.log(i)
+    }
+} */
+
+// Challenge 3
+
+/* for(i = 4; i >= -3.5; i+= -1.5) {
+    console.log(i)
+} */
+
+// Challenge 4
+
+
